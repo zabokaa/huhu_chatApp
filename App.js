b006bcdc7a9f9a727e643064e32f3be54e744c2c
zabokaa <>
2023-07-16 16:21:53 +0200

    exc.. button

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
-import { StyleSheet, View, TextInput, Text } from 'react-native';
+import { StyleSheet, View, TextInput, Text, Button, Alert} from 'react-native';
 import { useState } from 'react';
 
 
 
 const App = () => {
   const [text, setText] = useState('');
+  const alertMyText = () => {
+    Alert.alert(text);
+  }
+
   return (
     <View style={styles.container}>      
       <TextInput 
@@ -14,6 +18,10 @@ const App = () => {
        placeholder='Type Something Here'
      />
     <Text style={styles.textDisplay}>you wrote: {text}</Text>
+    <Button onPress={() => {
+        alertMyText();
+        }}
+        title='press me' />
     </View>
   );
 }
