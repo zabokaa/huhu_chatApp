1c7591748b1d1cb2233d64a6edb6daf7da10485b
zabokaa <>
2023-07-17 11:00:08 +0200

    Added  2flexboxes

--- components/Start.js
+++ components/Start.js
@@ -1,74 +1,104 @@
 import { useState } from 'react';
-import { StyleSheet, View, Text, Button, TextInput, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, Text, TextInput, TouchableOpacity, ImageBackground } from 'react-native';
 
 const Start = ({ navigation }) => {
-  const [name, setName] = useState('');
+    const [name, setName] = useState('');
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>HUHU</Text>
-      <TextInput
-        style={styles.yourName}
-        value={name}
-        onChangeText={setName}
-        placeholder='&#128100; Your Name'
-      />
-      <Text style={styles.chooseCol}>
-        Choose Background Color:
-      </Text>
-      <TouchableOpacity 
-        style={styles.chatButton} 
-        onPress={() => navigation.navigate('Chat', {name: name})}
-        >
-        <Text style={styles.btnText}>Start Chatting</Text>
-      </TouchableOpacity>
+        {/* <ImageBackground
+            source={require('../assets/Background Image.png')}
+            resizeMode='cover'
+            style={styles.backgroundImage}
+        > */}
+            <View style={styles.titleBox}>
+                <Text style={styles.title}>HUHU</Text>
+            </View>
+
+            <View style={styles.boxBack}>
+                <TextInput
+                    style={styles.yourName}
+                    value={name}
+                    onChangeText={setName}
+                    placeholder='&#128100; Your Name'
+                />
+                <Text style={styles.chooseCol}>
+                    Choose Background Color:
+                </Text>
+                <TouchableOpacity 
+                    style={styles.chatButton} 
+                    onPress={() => navigation.navigate('Chat', {name: name})}
+                    >
+                    <Text style={styles.btnText}>Start Chatting</Text>
+                </TouchableOpacity>
+            </View>
+           
+        {/* </ImageBackground>    */}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-     justifyContent: 'center',
-    alignItems: 'center'
-  },
-  title: {
-    fontSize: 45,
-    fontWeight: 600,
-    color: '#FFFFFF'
-  },
-  yourName: {
-    width: '88%',
-    padding: 15,
-    borderWidth: 1,
-    marginTop: 15,
-    marginBottom: 15,
-    fontSize: 16,
-    color: '#FFFFFF',
-    opacity: 50,
-  },
-  chooseCol:{
-    width: '88%',
-    fontSize: 16,
-    fontWeight: 300,
-    color: '#757083',
-    opacity: 100,
-    marginTop: 15,
-    marginBottom: 15,
-  },
-  chatButton: {
-    width: '88%',
-    backgroundColor: '#757083',
-    alignItems: 'center',
-    padding: 15,
-    marginTop: 15,
-    marginBottom: 15,
-  },
-  btnText: {
-    fontSize: 16,
-    fontWeight: 600,
-    color: '#FFFFFF'
-  }
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    backgroundImage: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    titleBox: {
+        flex: 56,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    boxBack: {
+        flex: 44,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'white',
+        width: '88%',
+        marginBottom: 20,
+    },
+    title: {
+        fontSize: 45,
+        fontWeight: 600,
+        color: '#FFFFFF'
+    },
+    yourName: {
+        width: '88%',
+        padding: 15,
+        borderWidth: 1,
+        marginTop: 15,
+        marginBottom: 15,
+        fontSize: 16,
+        color: '#FFFFFF',
+        opacity: 50,
+    },
+    chooseCol:{
+        width: '88%',
+        fontSize: 16,
+        fontWeight: 300,
+        color: '#757083',
+        opacity: 100,
+        marginTop: 15,
+        marginBottom: 15,
+    },
+    chatButton: {
+        width: '88%',
+        backgroundColor: '#757083',
+        alignItems: 'center',
+        padding: 15,
+        marginTop: 15,
+        marginBottom: 15,
+    },
+    btnText: {
+        fontSize: 16,
+        fontWeight: 600,
+        color: '#FFFFFF'
+    }
 });
 
 export default Start;
