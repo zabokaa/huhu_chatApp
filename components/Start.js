b53dd8400d8a8d0223df2b2c5859eff8fefaf29f
zabokaa <>
2023-07-17 12:41:25 +0200

    Added handleColorChange func

--- components/Start.js
+++ components/Start.js
@@ -3,9 +3,19 @@ import { StyleSheet, View, Text, TextInput, TouchableOpacity, ImageBackground } 
 
 const Start = ({ navigation }) => {
     const [name, setName] = useState('');
+    // user can change background color
+    const [backgroundColor, setBackgroundColor] = useState('');
+    // create circle around selected color button:
+    const [selectedCol, setSelectedCol] = useState('');
+
+    const handleColorChange = (color) => {
+        setBackgroundColor(color);
+        setSelectedCol(color);
+    };
+
 
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, {backgroundColor}]}>
         {/* <ImageBackground
             source={require('../assets/Background Image.png')}
             resizeMode='cover'
@@ -27,19 +37,35 @@ const Start = ({ navigation }) => {
                 </Text>
                 <View style={styles.colButtonContainer}>
                     <TouchableOpacity
-                        style={[styles.changeColButton, { backgroundColor: '#090C08' }]}
+                        style={[styles.changeColButton, { 
+                            backgroundColor: '#090C08',
+                            borderWidth: selectedCol==='#090C08' ? 3:0,
+                            borderColor: selectedCol==='#090C08' ? 'pink' : 'transparent',
+                        }]}
                         onPress={() => handleColorChange('#090C08')}
                     />
                     <TouchableOpacity
-                        style={[styles.changeColButton, { backgroundColor: '#474056' }]}
+                        style={[styles.changeColButton, { 
+                            backgroundColor: '#474056',
+                            borderWidth: selectedCol==='#474056' ? 3:0,
+                            borderColor: selectedCol==='#474056' ? 'pink' : 'transparent'
+                         }]}
                         onPress={() => handleColorChange('#474056')}
                     />
                     <TouchableOpacity
-                        style={[styles.changeColButton, { backgroundColor: '#8A95A5' }]}
+                        style={[styles.changeColButton, { 
+                            backgroundColor: '#8A95A5',
+                            borderWidth: selectedCol==='#8A95A5' ? 3:0,
+                            borderColor: selectedCol==='#8A95A5' ? 'pink' : 'transparent'
+                         }]}
                         onPress={() => handleColorChange('#8A95A5')}
                     />
                     <TouchableOpacity
-                        style={[styles.changeColButton, { backgroundColor: '#B9C6AE' }]}
+                        style={[styles.changeColButton, { 
+                            backgroundColor: '#B9C6AE',
+                            borderWidth: selectedCol==='#B9C6AE' ? 3:0,
+                            borderColor: selectedCol==='#B9C6AE' ? 'pink' : 'transparent',
+                        }]}
                         onPress={() => handleColorChange('#B9C6AE')}
                     />
                 </View>
@@ -109,6 +135,7 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'space-between',
         width: '88%',
+        marginBottom: 15,
       },
     changeColButton: {
         backgroundColor: '#090C08', 
